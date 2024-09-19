47bd0b9d48310d772ec368e62880e348a00fd07c
DoubleStrike <nikhil@doublestrike.com>
2024-09-19 12:36:40 -0400

    Print log message on start and comment cleanup

--- octoprint_segmentedbed/__init__.py
+++ octoprint_segmentedbed/__init__.py
@@ -1,19 +1,12 @@
 # coding=utf-8
 from __future__ import absolute_import
 
-### (Don't forget to remove me)
-# This is a basic skeleton for your plugin's __init__.py. You probably want to adjust the class name of your plugin
-# as well as the plugin mixins it's subclassing from. This is really just a basic skeleton to get you started,
-# defining your plugin as a template plugin, settings and asset plugin. Feel free to add or remove mixins
-# as necessary.
-#
-# Take a look at the documentation on what other plugin mixins are available.
-
 import octoprint.plugin
 
 class SegmentedbedPlugin(octoprint.plugin.SettingsPlugin,
     octoprint.plugin.AssetPlugin,
-    octoprint.plugin.TemplatePlugin
+    octoprint.plugin.TemplatePlugin,
+    octoprint.plugin.StartupPlugin
 ):
 
     ##~~ SettingsPlugin mixin
@@ -26,8 +19,7 @@ class SegmentedbedPlugin(octoprint.plugin.SettingsPlugin,
     ##~~ AssetPlugin mixin
 
     def get_assets(self):
-        # Define your plugin's asset files to automatically include in the
-        # core UI here.
+        # Asset files to automatically include in the core UI here.
         return {
             "js": ["js/segmentedbed.js"],
             "css": ["css/segmentedbed.css"],
@@ -55,17 +47,20 @@ class SegmentedbedPlugin(octoprint.plugin.SettingsPlugin,
                 "pip": "https://github.com/DoubleStrike/OctoPrint-SegmentedBed/archive/{target_version}.zip",
             }
         }
+        
+    ##~~ StartupPlugin mixin
+
+    def on_after_startup(self):
+        self._logger.info("Segmented bed plugin started up.")
 
 
 # If you want your plugin to be registered within OctoPrint under a different name than what you defined in setup.py
 # ("OctoPrint-PluginSkeleton"), you may define that here. Same goes for the other metadata derived from setup.py that
 # can be overwritten via __plugin_xyz__ control properties. See the documentation for that.
-__plugin_name__ = "Segmentedbed Plugin"
+__plugin_name__ = "Segmented Bed Plugin"
 
 
 # Set the Python version your plugin is compatible with below. Recommended is Python 3 only for all new plugins.
-# OctoPrint 1.4.0 - 1.7.x run under both Python 3 and the end-of-life Python 2.
-# OctoPrint 1.8.0 onwards only supports Python 3.
 __plugin_pythoncompat__ = ">=3,<4"  # Only Python 3
 
 def __plugin_load__():
