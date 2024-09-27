0abd613d928b1f7f5c352b55a2d2172ce501651c
larrox <145332912+larrrox@users.noreply.github.com>
2024-09-27 22:08:18 +1200

    optimize temp data handling
    small optimizations by streamlining the handling of temperature data
    
    **main changes:**
    - the dictionary for storing the tile temps seemed unnecessary, so it was removed, lmk if its there for a specific reason
    - resolved the TODO on line 70 regarding the sorting mechanism for remapping tile order, which should better performance.

--- octoprint_segmentedbed/static/js/segmentedbed.js
+++ octoprint_segmentedbed/static/js/segmentedbed.js
@@ -36,11 +36,12 @@ $(function() {
             T3:24.00/0.00 T4:23.00/0.00 T5:6.00/0.00 @:0 B@:0 HBR@:0 @0:0 @1:0 @2:0 @3:0 @4:0 @5:0 B_0_0:21.80/0.00 B_1_0:21.90/0.00 
             B_2_0:21.90/0.00 B_3_0:22.00/0.00 B_0_1:22.20/0.00 B_1_1:22.20/0.00 B_2_1:22.30/0.00 B_3_1:22.10/0.00 B_0_2:22.20/0.00 
             B_1_2:22.40/0.00 B_2_2:22.40/0.00 B_3_2:22.30/0.00 B_0_3:22.30/0.00 B_1_3:22.20/0.00 B_2_3:22.40/0.00 B_3_3:22.40/0.00
-
+            
             For reference, the T is active tool temperature, B is average bed temp, C is heated chamber temp, @ is heater power,
             HBR@ is heatbreak temp, T0-T5 are the toolhead temps.  Sourced from "lib/Marlin/Marlin/src/module/temperature.cpp"
             */
             
+
             // Get the portion of the line with temperatures, if the line is valid only
             if (newLogData.search("@5:0") < 10) return;
             var preParseData = newLogData.trim().split("@5:0 ")[1];
@@ -56,7 +57,6 @@ $(function() {
             // Increment thru every 3rd entry since there are 3 datapoints per tile (name, currentTemp, targetTemp)
             //      in that order. So we shorthand this by looping thru every 3 items and offsetting the indices.
             //      This is how we fill the dictionary.
-            var dictionaryOfTemps = {};
             self.heatbedTileArray.removeAll();
             
             // Build the array of information to use for the next step
@@ -64,23 +64,33 @@ $(function() {
                 let x = splitArray[i].substring(2, 3);
                 let y = splitArray[i].substring(4, 5);
                 let id = (3 - Number(y)) * 4 + Number(x) + 1;
-                dictionaryOfTemps[splitArray[i]] = { Current: splitArray[i + 1], Target: splitArray[i + 2], ID: id };
+                // Removed unnecessary dictionary for storing tile temperatures. 
+                self.heatbedTileArray.push({
+                    ID: id,
+                    Tile: splitArray[i],
+                    Current: splitArray[i + 1],
+                    Target: splitArray[i + 2],
+                    Style: ""
+                });
             }
 
-            // TODO: There has GOT to be a better way to do this sorting.
+            // Optimized sorting for remapping the tile order
             // Finally, remap the tile order to be correct and add to the ObservableArray
-            for (j = 0; j < reorderMatrix.length; j++) {
-                let tileName = splitArray[reorderMatrix[j]];
-                let currentTemp = splitArray[reorderMatrix[j] + 1];
-                let targetTemp = splitArray[reorderMatrix[j] + 2];
+            reorderMatrix.forEach(function(index) {
+                let tileName = splitArray[index];
+                let currentTemp = splitArray[index + 1];
+                let targetTemp = splitArray[index + 2];
                 let newStyle = "";
+
                 if (targetTemp == "0.00") {
                     newStyle = classOff;
-                } else if(Number(currentTemp) == Number(targetTemp)) {
+                } else if (Number(currentTemp) === Number(targetTemp)) {
                     newStyle = "";
                 } else {
                     newStyle = (Number(currentTemp) < Number(targetTemp)) ? classHeat : classCool;
                 }
+
+                // Use a single push to add the tile data
                 self.heatbedTileArray.push({
                     ID: self.heatbedTileArray().length,
                     Tile: tileName,
@@ -88,7 +98,7 @@ $(function() {
                     Target: targetTemp,
                     Style: newStyle
                 });
-            }
+            });
         }
     }
 
@@ -98,9 +108,9 @@ $(function() {
         construct: SegmentedbedViewModel,
 
         // ViewModels your plugin depends on, e.g. loginStateViewModel, settingsViewModel, ...
-        dependencies: [ "terminalViewModel" ],
+        dependencies: ["terminalViewModel"],
         
         // Elements to bind to, e.g. #settings_plugin_segmentedbed, #tab_plugin_segmentedbed, ...
-        elements: [ "#tab_plugin_segmentedbed" ]
+        elements: ["#tab_plugin_segmentedbed"]
     });
 });
